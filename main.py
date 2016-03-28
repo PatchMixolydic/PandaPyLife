48f262514e240fbad77d815e1a3a24b5e906721f
Sparkpin <stitchbilly@hotmail.com>
2016-03-28 17:35:44 -0500

    Edit Mode

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import random
 base = ShowBase()
 
 gridSize = 16 # Both dimensions of the grid. The grid will wrap.
-interval = 2
 
 loopRunning = False
+stepButton = None
 runButton = None
+editButton = None
+editMode = False
 grid = []
 
 class Cell:
@@ -18,15 +20,23 @@ class Cell:
         self.model = loader.loadModel('gfx/block')
         self.model.reparentTo(render)
         self.alive = alive
+        self.model.setTag('cell', '1')
 
     def draw(self):
         if self.alive:
             self.model.show()
+            self.model.setColorScale(1,1,1,1)
         else:
-            self.model.hide()
+            if editMode:
+                self.model.show()
+                self.model.setColorScale(0.5,0.5,0.5,1)
+            else:
+                self.model.hide()
 
 def initialize():
+    global stepButton
     global runButton
+    global editButton
 
     # Camera setup
     base.disableMouse()
@@ -34,8 +44,9 @@ def initialize():
     base.camera.setP(270)
 
     # DirectGui
-    stepButton = DirectButton(text = ("Step", "Step", "Step", "Step"), scale = 0.05, command = step, pos = (-0.25, 0, 0.75))
-    runButton = DirectButton(text = ("Run", "Run", "Run", "Run"), scale = 0.05, command = toggleRun, pos = (0.25, 0, 0.75))
+    stepButton = DirectButton(text = ("Step", "Step", "Step", "Step"), scale = 0.05, command = step, pos = (-0.33, 0, 0.75))
+    runButton = DirectButton(text = ("Run", "Run", "Run", "Run"), scale = 0.05, command = toggleRun, pos = (0, 0, 0.75))
+    editButton = DirectButton(text = ("Edit", "Edit", "Edit", "Edit"), scale = 0.05, command = toggleEdit, pos = (0.33, 0, 0.75))
 
     # Create a 2D array, so that a cell may be looked at by using [x][y]. Also create initial grid
     for x in xrange(0, gridSize):
@@ -82,6 +93,8 @@ def step():
             if y.alive:
                 if neighbors.count(True) < 2 or neighbors.count(True) > 3:
                     y.alive = False
+                else:
+                    y.alive = True
             elif neighbors.count(True) == 3:
                 y.alive = True
     draw()
@@ -100,6 +113,58 @@ def toggleRun():
 def runLoop(task):
     step()
     return task.cont
+
+def toggleEdit():
+    global editMode
+    if editMode:
+        editMode = False
+        editButton["text"] = ("Edit", "Edit", "Edit", "Edit")
+        stepButton["text"] = ("Step", "Step", "Step", "Step")
+        stepButton["command"] = step
+        runButton.show()
+    else:
+        editMode = True
+        editButton["text"] = ("Back", "Back", "Back", "Back")
+        stepButton["text"] = ("Clear", "Clear", "Clear", "Clear")
+        stepButton["command"] = clearGrid
+        runButton.hide()
+    draw()
+
+traverser = CollisionTraverser()
+handler = CollisionHandlerQueue()
+
+pickerNode = CollisionNode('mouseRay')
+pickerNP = camera.attachNewNode(pickerNode)
+pickerNode.setFromCollideMask(GeomNode.getDefaultCollideMask())
+pickerRay = CollisionRay()
+pickerNode.addSolid(pickerRay)
+traverser.addCollider(pickerNP, handler)
+
+def handlePick():
+    if not editMode:
+        return
+
+    if base.mouseWatcherNode.hasMouse():
+        mpos = base.mouseWatcherNode.getMouse()
+        pickerRay.setFromLens(base.camNode, mpos.getX(), mpos.getY())
+
+        traverser.traverse(render)
+        if handler.getNumEntries() > 0:
+            handler.sortEntries()
+            pickedObj = handler.getEntry(0).getIntoNodePath()
+            pickedObj = pickedObj.findNetTag('cell')
+            if not pickedObj.isEmpty():
+                cell = grid[int(pickedObj.getX())][int(pickedObj.getY())]
+                cell.alive = not cell.alive
+                cell.draw()
+
+def clearGrid():
+    for row in grid:
+        for cell in row:
+            cell.alive = False
+    draw()
+
+base.accept('mouse1', handlePick)
 
 initialize()
 base.run()
